6b2ac544ff7d36baf0386b12562cd5b1a0decb1a
Kate <sokoloffkate@gmail.com>
2022-05-06 17:16:57 +1000

    Added id to input fields and changed js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const popupCloseBut = document.querySelector('.popup__close-button');
 let form = document.querySelector('.popup__form')
 let profileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__subtitle');
-let inputName = form.querySelector('.popup_name');
-let inputJob = form.querySelector('.popup_job');
+let inputName = form.querySelector('#popup_name');
+let inputJob = form.querySelector('#popup_job');
 
 function openPopup() {
  popup.classList.add('popup_opened');
